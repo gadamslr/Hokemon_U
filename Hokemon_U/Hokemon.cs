a10ab157859c5f1f2efcf198b6eff4abd3129bbb
Gareth Adams <gadams@longroad.ac.uk>
2021-01-14 12:23:25 +0000

    Add project files.

---
+++ Hokemon_U/Hokemon.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hokemon_U
+{
+    class Hokemon
+    {
+
+        // MEMBERS of a CLASS include Attributes, Properties and Methods
+
+        // Attributes
+        private string name;
+        private int health;
+        private int max_health;
+        private int attack;
+        private int speed;
+        private int defense;
+
+        // Properties
+
+        public string Name
+        {
+            get { return name; }   // access to the name attribute
+            set { name = value; }  // sets the name attribute
+        }
+        public int Health
+        {
+            get { return health; }   // access to the health attribute
+            set { health = value; }
+        }
+
+        // Methods
+
+        public Hokemon()  // CONSTRUCTOR method runs 1st when INSTANTIATED
+        {
+            provide_name();
+            max_health = 100;
+            health = random_int_generator(10, max_health);
+            attack = random_int_generator(10, 100);
+            speed = random_int_generator(10, 100);
+            defense = random_int_generator(10, 100);
+        }
+
+        public void provide_name() // METHOD
+        {
+            Console.WriteLine("Enter a name for your Hokemon: ");
+            name = Console.ReadLine();
+        }
+
+        public void get_details()
+        {
+            Console.WriteLine("\n\n*********\nName: {0}\n" +
+                "Health: {1}/{2}\n" +
+                "Attack value: {3}\n" +
+                "Speed value: {4}\n" +
+                "Defense value: {5}", name, Health, max_health, attack, speed, defense);
+        }
+
+        public int random_int_generator(int min_value, int max_value) // int min_value, int max_value are PARAMETERS
+        {
+            Random rnd = new Random();    // Creating a new OBJECT called rnd this is an 
+                                                    // INSTANCE of RANDOM class
+
+            int randomValue;
+
+            randomValue = rnd.Next(min_value, max_value);  // Usine the NEXT method from the RANDOM class found in rnd OBJECT
+
+            return randomValue;
+        }
+    }
+}
---
+++ Hokemon_U/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Hokemon_U
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello to Hokeworld!");
+
+            Hokemon hokeObject01 = new Hokemon();
+
+            //Console.WriteLine("Hokemon name is: {0}", hokeObject01.Name);
+
+            hokeObject01.get_details();
+
+            Hokemon hoke02 = new Hokemon();
+
+            //Console.WriteLine("Hokemon name is: {0}", hokeObject01.Name);
+
+            hoke02.get_details();
+
+
+
+        }
+    }
+}
