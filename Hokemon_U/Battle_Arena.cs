69ea08c3de4f3ee7d996603d5fcc3486b1b5da1b
Gareth Adams <gadams@longroad.ac.uk>
2021-01-15 14:18:14 +0000

    Added Battle_Arena class

---
+++ Hokemon_U/Battle_Arena.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hokemon_U
+{
+    class Battle_Arena
+    {
+        public Battle_Arena() // CONSTRUCTOR method runs 1st
+        {
+            Console.WriteLine("Welcome to the Battle Arena!!!!");
+
+        }
+
+        public void ChallengeMe(Hokemon hokeChallenge)  // Passing a Hoke Object into Method
+                                                            //to use in the method
+        {
+            Console.WriteLine("{0}: says ' I want a BATTLE!!!!!'", hokeChallenge.Name);
+
+        }
+
+        public void ChallengeAccepted(Hokemon Challenger, Hokemon Accepter)
+        {
+            Console.WriteLine("{0}: 'I accept you challenge for a fight'" +
+                "Come on then {1} lets RUMBLE!!",Accepter.Name, Challenger.Name);
+        }
+
+    }
+}
--- Hokemon_U/Program.cs
+++ Hokemon_U/Program.cs
@@ -18,6 +18,9 @@ namespace Hokemon_U
 
             //Console.WriteLine("Hokemon name is: {0}", hokeObject01.Name);
 
+            Console.WriteLine("Name of Hokemon: {0}",hoke02.Name);
+            Console.WriteLine("***********");
+            Console.WriteLine("****Details below***");
             hoke02.get_details();
 
             Hokemon hoke03 = new Hokemon();  // INSTANTIATION of object 2
@@ -25,6 +28,13 @@ namespace Hokemon_U
             //Console.WriteLine("Hokemon name is: {0}", hokeObject01.Name);
 
             hoke03.get_details();
+
+            Battle_Arena firstArena = new Battle_Arena(); // INSTANTIATED Battle_Arena
+
+            firstArena.ChallengeMe(hokeObject01);
+
+            firstArena.ChallengeAccepted(hokeObject01, hoke03); // Passing two objects into 
+                                                                // the firstArena
 
         }
     }
